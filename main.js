741c7228ae1bbdda1df43580512080cc789f4663
Yogesh Wadhankar <yogeshwadhankar4433@gmail.com>
2022-09-08 14:50:37 +0530

    commit 1

---
+++ main.js
@@ -0,0 +1,8 @@
+
+const name = 'Yogesh';
+const age =20;
+const add = 'abc';
+console.log("Personal Details =My name is "+name+" age is "+age+" Address is "+add);
+
+var s ='My name is Yogesh, age is 22, Address is abc';
+console.log(s.split(','))
