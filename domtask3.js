74ec221cfba004cd3793ea52a7d20a280b917b52
Yogesh Wadhankar <yogeshwadhankar4433@gmail.com>
2022-09-09 08:15:23 +0530

    domtask no 4

--- domtask3.js
+++ domtask3.js
@@ -5,7 +5,8 @@ headertitle.textContent='Hello'
 // console.log(headertitle)
 headertitle.style.borderBottom ='solid 3px #000';
 var item =document.getElementsByClassName('list-group-item');
-console.log(item)
+console.log(item);
+
 item[0].textContent ='Hello'
-item[1].style.fontWeigth ='bold';
-item[1].style.backgroundColor ='yellow';
+item[1].style.fontWeight='bold';
+item[2].style.backgroundColor ='green';
