53063cc00496c77a19d0ce0593818f6b394bc082
Yogesh Wadhankar <yogeshwadhankar4433@gmail.com>
2022-09-09 09:24:40 +0530

    dom task no 6

--- domtask3.js
+++ domtask3.js
@@ -10,3 +10,12 @@ console.log(item);
 item[0].textContent ='Hello'
 item[1].style.fontWeight='bold';
 item[2].style.backgroundColor ='green';
+var header =document.querySelector('#main-header')
+header.style.borderBottom='solid 4px #ccc';
+var itemlast =document.querySelector('.list-group-item:last-Child');
+itemlast.style.color='green';
+var odd =document.querySelectorAll('li:nth-Child(odd)');
+for(var i=0;i<odd.length;i++)
+{
+    odd[i].style.backgroundColor='#f4f4f4';
+}
