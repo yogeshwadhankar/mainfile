a904c38edb2530c18fa84d244a99f8bfab3e8d03
Yogesh Wadhankar <yogeshwadhankar4433@gmail.com>
2022-09-11 09:13:14 +0530

    domtask9

---
+++ domtask9.js
@@ -0,0 +1,11 @@
+var form =document.getElementById('addforms');
+var itemList =document.getElementById('items');
+form.addEventListener('submit',addItems);
+function addItems(e)
+{
+    e.preventDefault();
+    var newitems =document.getElementById('item');
+
+}
+const descriptionNode =document.createTextNode(description);i
+li.appendChild();
